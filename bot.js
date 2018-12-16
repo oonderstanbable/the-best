ce198edb2aae3961c10851992a46ce32c6feace3
oonderstanbable <45098109+oonderstanbable@users.noreply.github.com>
2018-12-16 08:42:40 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = 'OK GOOGLE'
 
 client.on('ready', () => {
     console.log('Google Home!');
 });
 
 client.on('message', message => {
-    let msg = message.content.toUpperCase()
-    
-    if (msg.content === prefix) {
-        message.channel.send('Hello, google home is at your service!')
-    }
-    
-    if (msg.content === prefix + ' WHAT IS THE WEATHER LIKE TODAY?') {
-        message.channel.send('https://www.accuweather.com/%27')
-    }
-})
+    if (message.content === 'ok google') {
+        message.channel.send('Hello google home is at your service');
+      }
+});
 
+client.on('message', message => {
+    if (message.content === 'ok google what is the weather like today?') {
+        message.reply('https://www.accuweather.com/');
+      }
+});
+
+
+client.on('message', message => {
+    if (message.content === 'ok google when does OOOPS have a birthday?') {
+        message.reply('OOOPS has a birthday on November 3rd every year... like your birthday should be...');
+      }
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
