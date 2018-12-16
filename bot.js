b152931268834430b06acc4981c1fd745c7b7b8c
oonderstanbable <45098109+oonderstanbable@users.noreply.github.com>
2018-12-16 08:31:08 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = 'OK GOOGLE'
 
 client.on('ready', () => {
     console.log('Google Home!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ok google') {
-    	message.channel.send('Hello google home is at your service');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'ok google what is the weather like today?') {
-    	message.reply('https://www.accuweather.com/');
-  	}
-});
+    let msg = message.content.toUpperCase()
+    
+    if (msg.content === prefix) {
+        message.channel.send('Hello, google home is at your service!')
+    }
+    
+    if (msg.content === prefix + ' WHAT IS THE WEATHER LIKE TODAY?') {
+        message.channel.send('https://www.accuweather.com/%27')
+    }
+})
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
