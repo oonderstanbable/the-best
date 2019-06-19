40bb8011bb1023e3da51a260c64a9d25e1c3147a
OOOPS <45098109+oonderstanbable@users.noreply.github.com>
2019-06-19 18:22:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const { Client, Attachment } = require('discord.js');
 const client = new Discord.Client();
 //const KrunkerJS = require('krunker.io');
 //const Krunker = new KrunkerJS();
