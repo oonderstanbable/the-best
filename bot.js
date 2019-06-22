ca71828bc02e057318ccb98f0d86bea92740b139
OOOPS <45098109+oonderstanbable@users.noreply.github.com>
2019-06-21 23:58:59 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -70,7 +70,7 @@ message.channel.sendEmbed(commands);
 });
 let clan = new Discord.RichEmbed()
 .setColor(15844367)
-.addField("**Clan Level**", "87")
+.addField("**Clan Level**", "88")
 .addField("**Clan Members:**", "OOOPS (leader)\n"
 + "awesompuppy\n"
 + "boB_\n"
