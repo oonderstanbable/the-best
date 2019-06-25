44b845bbcef002cb6f141f9f1c7b3cc9dd006342
OOOPS <45098109+oonderstanbable@users.noreply.github.com>
2019-06-25 08:28:23 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,14 +16,14 @@ function cL(a){return a.replace(/[\[\]]/g, "");}
 function tL(a){return a.toLowerCase();}
 
 
-
+client.on('guildMemberAdd', member => {
 let join = new Discord.RichEmbed()
 .setColor(15158332)
 .addField('**' + member.user.username + '**, has joined era Clan discord')  
 .addField("use **era help* to see era Bot commands", "If you want to apply you can go to #【📋】how-to-apply to learn join")
     console.log('User' + member.user.username + 'Has joined the server!')
     console.log(member)
-client.on('guildMemberAdd', member => {
+
     member.guild.channels.get('586309740217237525').sendEmbed(join);
 });
 /*client.on('message', message => {
