585b81b3bace9584a8c9e6f3d47a5e1d4d7b1516
OOOPS <45098109+oonderstanbable@users.noreply.github.com>
2019-08-22 08:07:30 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,22 +17,15 @@ function iN(a){return/^\d+$/.test(a);}function iO(a){return a.match('^[/*+-]{1}$
 function cL(a){return a.replace(/[\[\]]/g, "");}
 function tL(a){return a.toLowerCase();}
 
-/*
 client.on('guildMemberAdd', member => {
 
 let join = new Discord.RichEmbed()
 .setColor(15158332)
-.addField('**' + member.user.username + '**, has joined era Clan discord', 'Make sure to Follow https://krunker.io/social.html?p=profile&q=OOOPS') 
-.addField("Use **__era help__** to see era Bot commands", "If you want to apply you can go to #【📋】how-to-apply to learn join")
+.addField("Use **__.help__** to see RCS Bot commands", "Make sure to follow the rules")
     console.log('User' + member.user.username + 'Has joined the server!')
-    console.log(member)
-    
-    message.guildMember.sendEmbed(rules);
-    member.guild.channels.get('586309740217237525').sendEmbed(join);
+    console.log(Validated)
+    member.guild.channels.get('614083118667923457').sendEmbed(join);
 });
-client.on('message', message => {
-let args = message.content.split(' ');*/
-
 
 client.on('message', message => {
     if (tL(message.content) === '.avatar') {
@@ -50,11 +43,11 @@ message.channel.sendEmbed(invite);
 });
 let commands = new Discord.RichEmbed()
 .setColor(15844367)
-.addField("Commands Available:", "-.avatar\n"
-+ "-.help\n"
-+ "-.flip\n"
-+ "-.rcs\n"
-+ "-.invite")
+.addField("Commands Available:", ".avatar\n"
++ ".help\n"
++ ".flip\n"
++ ".rcs\n"
++ ".invite")
 client.on('message', message => {
 if (tL(message.content) === '.help') {
 message.channel.sendEmbed(commands);
