d2c53f0d40f2f60afb1d96e75fbaa181348bd9f3
OOOPS <45098109+oonderstanbable@users.noreply.github.com>
2020-06-10 16:31:31 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,7 +45,8 @@ client.on('message', message => {
   });
 let scrims = new Discord.RichEmbed()
 .setColor(15158332)
-.addField("‎" , "**__Smoke E-Sports Scrim Schduele__**\n")  
+.addField("‎" , "_________________\n" 
++ "**__Smoke E-Sports Scrim Schduele__**\n")  
 client.on('message', message => {
 if (tL(message.content) === '.scrims'){
 message.channel.sendEmbed(scrims);
