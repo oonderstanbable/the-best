4eed8cbca06ae5d108dd018e6607d8f8cf2c8ff5
OOOPS <45098109+oonderstanbable@users.noreply.github.com>
2020-06-10 19:28:03 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -108,27 +108,21 @@ if (tL(message.content) === '.server') {
 if (tL(message.content) === '.3v3') {
 message.channel.send(three());
     }
-if(tL(message.content) === '.kick'){
-    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
+  if(tL(message.content) === ".p") {
+    // This command removes all messages from all users in the channel, up to 100.
     
-    // Let's first check if we have a member and if we can kick them!
-    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-    // We can also support getting the member by ID, which would be args[0]
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+    // get the delete count, as an actual number.
+    const deleteCount = parseInt(args[0], 10);
     
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
+    // Ooooh nice, combined conditions. <3
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
     
-    // Now, time for a swift kick in the nuts!
-    await member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
-}
+    // So we get our messages, and delete them. Simple enough, right?
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
 if (tL(message.content) === '.2v2') {
 message.channel.send(two());
     }
